c0b744cb39aceb83b986cfb037fbba910fd16d6a
valeria56467 <valeria56467@users.noreply.github.com>
2020-05-05 16:11:13 +0200

    Завершение функции авторизации

--- js/main.js
+++ js/main.js
@@ -14,43 +14,74 @@ function toggleModal() {
 
 //console.log(document.querySelector('.button-auth'));
 
-const buttouAuth = document.querySelector('.button-auth');
+const buttonAuth = document.querySelector('.button-auth');
 const modalAuth = document.querySelector('.modal-auth');
 const closeAuth = document.querySelector('.close-auth'); // объявляем константой кнопку "закрытия" окна авторизации
 const logInForm = document.querySelector('#logInForm'); //получение элемента формы авторизации
 const loginInput = document.querySelector('#login');
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
 
-let login = ''; //создаем переменную для проверки авторизации по которой у нас будет идти проверка
-               //console.dir(modalAuth);
+
+
+
+let login = localStorage.getItem('gloDelivery'); //создаем переменную для проверки авторизации по которой у нас будет идти проверка, придаем ей значение сохранения
+
 
 function toggleModalAuth() { //функция вызова окна авторизации
   modalAuth.classList.toggle('is-open')
 }
-/*buttouAuth.addEventListener('click', function () {
-  console.log('Hello');
-});*/
 
 
-                                              //buttouAuth.removeEventListener('click', toggleModalAuth) функция удаления
+
 function authorized() {
-  console.log('Авторизован');
+    function logOut () { //эта функция обнуляет наш логин
+        login = null;
+        localStorage.removeItem('gloDelivery');
+        buttonAuth.style.display = ''; //отображаем кнопку "Войти"
+        userName.style.display = ""; //скрываем имя пользователя
+        buttonOut.style.display = ""; //скрываем кнопку Выход
+        buttonOut.removeEventListener('click', logOut)
+        checkAuth();
+    }
 
+  console.log('Авторизован');
+  userName.textContent = login //свойство которое содержит текст данного элемента, мыы можем не только получать но и записывать
+ // мы можем менять стили css, у примеру -  buttonAuth.style.backgroundColor = 'red'  это изменение фона кнопки "Войти"
+    buttonAuth.style.display = 'none'; //скрываем отображение кнопки "Войти", когда вход уже осуществлен
+    userName.style.display = "inline"; //показываем имя пользователя
+    buttonOut.style.display = "block";//показываем кнопку "Выход"
+    buttonOut.addEventListener('click', logOut) //на кнопку "Выход" добавляем событие
 }
+
  function notAuthorized() {
   console.log('Не авторизован');
   function logIn (event) {
-    event.preventDefault() //не работает
-    console.log('Логин');
-    console.log(loginInput.value);
+    event.preventDefault();
+    login = loginInput.value;
+    localStorage.setItem('gloDelivery', login) //сохраняем имя пользователя, что бы не надо было вводить после перезагрузки страницы
+    toggleModalAuth();
+    buttonAuth.removeEventListener('click', toggleModalAuth); //установка события по клику мышки - открытия окна
+      closeAuth.removeEventListener('click', toggleModalAuth); // установка события - закрытия окна по знаку "Х"
+      logInForm.removeEventListener('submit', logIn);
+      logInForm.reset (); //очищаем окно ввода логина
+    checkAuth()
+
   }
 
-   buttouAuth.addEventListener('click', toggleModalAuth); //установка события по клику мышки - открытия окна
+   buttonAuth.addEventListener('click', toggleModalAuth);
    closeAuth.addEventListener('click', toggleModalAuth); // установка события - закрытия окна по знаку "Х"
-   logInForm.addEventListener('submin', logIn);
+   logInForm.addEventListener('submit', logIn);
  }
 
-if (login) {
-  authorized();
-  } else {
-  notAuthorized();
-}
+ function checkAuth() {
+     if (login) {
+         authorized();
+     } else {
+         notAuthorized();
+     }
+
+ }
+
+ checkAuth();
+
