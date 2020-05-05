3cd91e956e50aea0a6314758cf811b889ce8a139
valeria56467 <valeria56467@users.noreply.github.com>
2020-05-05 14:09:42 +0200

    event.preventDefault() //не работает

--- js/main.js
+++ js/main.js
@@ -12,8 +12,46 @@ function toggleModal() {
 //day 1
 //реализация авторизации
 
-console.log(document.querySelector('.button-auth'))
+//console.log(document.querySelector('.button-auth'));
 
-const buttouAuth = document.querySelector('.button-auth')
-const modalAuth = document.querySelector('.modal-auth')
-console.log(buttouAuth)
+const buttouAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth'); // объявляем константой кнопку "закрытия" окна авторизации
+const logInForm = document.querySelector('#logInForm'); //получение элемента формы авторизации
+const loginInput = document.querySelector('#login');
+
+let login = ''; //создаем переменную для проверки авторизации по которой у нас будет идти проверка
+               //console.dir(modalAuth);
+
+function toggleModalAuth() { //функция вызова окна авторизации
+  modalAuth.classList.toggle('is-open')
+}
+/*buttouAuth.addEventListener('click', function () {
+  console.log('Hello');
+});*/
+
+
+                                              //buttouAuth.removeEventListener('click', toggleModalAuth) функция удаления
+function authorized() {
+  console.log('Авторизован');
+
+}
+ function notAuthorized() {
+  console.log('Не авторизован');
+
+  function logIn (event) {
+    event.preventDefault() //не работает
+    console.log('Логин');
+    console.log(loginInput.value)
+  }
+
+   buttouAuth.addEventListener('click', toggleModalAuth); //установка события по клику мышки - открытия окна
+   closeAuth.addEventListener('click', toggleModalAuth); // установка события - закрытия окна по знаку "Х"
+   logInForm.addEventListener('submin', logIn)
+ }
+
+if (login) {
+  authorized();
+  } else {
+  notAuthorized();
+}
